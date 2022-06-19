cbb1ce783bb7558b0c10836b373f3bb0b02d1892
Chameleon <101677796+Chameleon-Coder@users.noreply.github.com>
2022-06-19 12:04:48 -0400

    Tips! (#85)
    * Tips!
    
    * Tip 2
    
    * Rename tip.js to draft-issues.js
    
    * Update draft-issues.js

---
+++ code/tips/dev-environment.rb
@@ -0,0 +1,4 @@
+puts "Hello, random stranger."
+puts "I am Chameleon, and I have a GitHub tip for you."
+puts "Press . to open a dev environment."
+puts "Thank you for your time."
---
+++ code/tips/draft-issues.js
@@ -0,0 +1,2 @@
+console.log;(Here is a GitHub tip.);
+console.log;(Got a beta project? You can make a draft Issue.);
