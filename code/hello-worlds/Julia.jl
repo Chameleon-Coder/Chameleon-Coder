b938b5523b4761bfc677d219310ac807913eacb1
Circle <101677796+Chameleon-Coder@users.noreply.github.com>
2022-07-06 08:39:18 -0400

    Create Julia.jl

---
+++ code/hello-worlds/Julia.jl
@@ -0,0 +1,13 @@
+julia> function foo()
+           x = "Hello, "; x *= "World!"
+           return x
+       end
+foo (generic function with 1 method)
+
+julia> bar() = (x = "Hello, Mars!"; return x)
+bar (generic function with 1 method)
+
+julia> foo();
+
+julia> bar()
+"Hello, Mars!"
