b5640506a93f9ec570eb00a3b7a9c6e520348143
Mergify <37929162+mergify[bot]@users.noreply.github.com>
2023-03-19 17:57:36 +0000

    big boi (#199) Update config.yml
    * Removed AI
    
    * lazy
    
    * lazy 2

--- code-dump/tips/draft-issues.js
+++
@@ -1,2 +0,0 @@
-console.log;(Here is a GitHub tip.);
-console.log;(Got a beta project? You can make a draft Issue.);
---
+++ code-dump/tips/lazy.rb
@@ -0,0 +1,4 @@
+puts "Hello, random stranger."
+puts "I am Chameleon, and I have a GitHub tip for you."
+puts "Too lazy to squash commits yourself? Get an action called Mergify."
+puts "Thank you for your time."
